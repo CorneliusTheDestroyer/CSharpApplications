d140785f834c2a7f441ba64ae2ab0938a449e3df
Corne Stols <cornestols7@gmail.com>
2025-04-23 14:37:37 +0200

    New Commit

---
+++ Practice Apps/PracticeApp07 - Interfaces/PracticeApp07 - Interfaces/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace PracticeApp07___Interfaces
+{
+    public interface IEnemy
+    {
+        void Attack();
+        void Defend();
+    }
+
+    public class Goblin : IEnemy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Goblin attacks!");
+        }
+        public void Defend()
+        {
+            Console.WriteLine("Goblin defends!");
+        }
+    }
+
+    public class Troll : IEnemy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Troll attacks!");
+        }
+        public void Defend()
+        {
+            Console.WriteLine("Troll defends!");
+        }
+    }
+
+    public class Dragon : IEnemy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Dragon attacks!");
+        }
+        public void Defend()
+        {
+            Console.WriteLine("Dragon defends!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnemy goblin = new Goblin();
+            IEnemy troll = new Troll();
+            IEnemy dragon = new Dragon();
+
+            //goblin.Attack();
+            //goblin.Defend();
+            //troll.Attack();
+            //troll.Defend();
+            //dragon.Attack();
+            //dragon.Defend();
+
+            List<IEnemy> enemies = new List<IEnemy>();
+
+            enemies.Add(goblin);
+            enemies.Add(troll);
+            enemies.Add(dragon);
+            
+            foreach (IEnemy enemy in enemies)
+            {
+                enemy.Attack();
+                enemy.Defend();
+            }
+        }
+    }
+}
