5a81f5e9a4cea8b1152f7775e6b76467c5c0e0fb
Corne Stols <cornestols7@gmail.com>
2025-11-18 19:26:40 +0200

    New Commit

---
+++ Practice Apps/PracticeApp12/ConsoleApp1/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Customer
+    {
+        //Member Variable
+        public int _Id = 0;
+
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //Constructors
+        public Customer(string name) 
+        {
+            Name = name;
+            Console.WriteLine($"Customer {Name} created");
+        }
+
+        public Customer()
+        {
+            Name = "Defaut Name";
+            Console.WriteLine($"Customer {Name} created");
+        }
+
+        public Customer(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine($"Customer {Name} created with an age of {Age}");
+        }
+
+        public void Register()
+        {
+            Console.WriteLine($"Customer {Name} successfully registered");
+        }
+
+        public void Register(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine($"Customer {Name} with an age of {Age} successfully registered");
+        }
+
+    }
+}
+ 
---
+++ Practice Apps/PracticeApp12/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Customer customer = new Customer();
+            Customer customer1 = new Customer("John");
+            Customer customer2 = new Customer("Mike");
+            Customer customer3 = new Customer("Dan", 23);
+
+            customer._Id = 232323;
+
+            Console.WriteLine($"Customer has an ID of {customer._Id}");
+
+            customer.Register(); 
+            customer1.Register();
+            customer2.Register();
+            customer3.Register();
+            customer3.Register("Micheal", 43);
+        }
+    }
+}
