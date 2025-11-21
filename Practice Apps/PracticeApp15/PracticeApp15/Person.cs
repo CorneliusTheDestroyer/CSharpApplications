e3a418cfd293f0a650ef9ffc8148ee9a42e19dd5
Cornelius <cornestols7@gmail.com>
2025-11-21 17:04:04 +0200

    New Commit

---
+++ Practice Apps/PracticeApp15/PracticeApp15/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace PracticeApp15
+{
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            Age = age;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"Hello, my name is {_name} and I am {_age} years old.");
+        }
+    }
+}
---
+++ Practice Apps/PracticeApp15/PracticeApp15/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticeApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", 30);
+            person.Greet();
+        }
+    }
+}
