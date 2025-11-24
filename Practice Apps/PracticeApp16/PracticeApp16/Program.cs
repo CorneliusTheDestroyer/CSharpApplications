efbf90d2cbff79f9a2a23db80b1978747c29840e
Cornelius <cornestols7@gmail.com>
2025-11-24 11:21:21 +0200

    New Commit

--- Practice Apps/PracticeApp16/PracticeApp16/Program.cs
+++ Practice Apps/PracticeApp16/PracticeApp16/Program.cs
@@ -9,7 +9,8 @@
             //person.Display();
             Person.Display();
 
-            var myNamesList = new List<string> { "Test01", "Test02", "Test03" };
+            //var myNamesList = new List<string> { "Test01", "Test02", "Test03" };
+            List<string> myNamesList = new List<string> { "Test01", "Test02", "Test03" };
 
             myNamesList.Add(person.Name);
             myNamesList.Remove("Test02");
@@ -18,6 +19,17 @@
             foreach (var name in myNamesList)
             {
                 Console.WriteLine($"Names: {name}");
+            }
+
+            var people = new List<Person>
+            {
+                new Person("Mike", 28),
+                new Person("Bryan", 23)
+            };
+
+            foreach (var per in people)
+            {
+                Console.WriteLine($"Person: {per.Name}, Age: {per.Age}");
             }
         }
     }
