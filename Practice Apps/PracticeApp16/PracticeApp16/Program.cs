9dcba89f56a38d4763d652bd0ad44040bf31cf98
Cornelius <cornestols7@gmail.com>
2025-11-24 10:35:40 +0200

    New Commit

--- Practice Apps/PracticeApp16/PracticeApp16/Program.cs
+++ Practice Apps/PracticeApp16/PracticeApp16/Program.cs
@@ -8,6 +8,17 @@
 
             //person.Display();
             Person.Display();
+
+            var myNamesList = new List<string> { "Test01", "Test02", "Test03" };
+
+            myNamesList.Add(person.Name);
+            myNamesList.Remove("Test02");
+            myNamesList.Sort();
+
+            foreach (var name in myNamesList)
+            {
+                Console.WriteLine($"Names: {name}");
+            }
         }
     }
 }
