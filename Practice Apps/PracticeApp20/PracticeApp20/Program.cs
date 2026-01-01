6ddc9385e3f315a04a8de1dab5cf439c5cafcd35
Corne Stols <cornestols7@gmail.com>
2026-01-01 10:49:24 +0200

    New Commit

---
+++ Practice Apps/PracticeApp20/PracticeApp20/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Trying...");
+            }
+            catch 
+            {
+                Console.WriteLine("Error found.");
+            }
+            finally
+            {
+                Console.WriteLine("Finally executed."); // Code to execute regardless of exception
+            }
+        }
+    }
+}
---
+++ Practice Apps/PracticeApp21/PracticeApp21/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public void HandleMultipleExceptions(string numberString, int index)
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                int number = int.Parse(numberString);
+                Console.WriteLine(numbers[index]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range.");
+            }
+        }
+    }
+}
