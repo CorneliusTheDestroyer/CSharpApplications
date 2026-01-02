3579bdf17417e025ea0dffead1a304e8998a6cb5
Corne Stols <cornestols7@gmail.com>
2026-01-02 11:37:00 +0200

    New Commit

---
+++ Practice Apps/PracticeApp22/PracticeApp22/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PracticeApp22
+{
+    public class Animal
+    {
+        public virtual void MakeSounds()
+        {
+            Console.WriteLine("Animal sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSounds() 
+        {
+            base.MakeSounds();
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+
+            myDog.MakeSounds();
+        }
+    }
+}
