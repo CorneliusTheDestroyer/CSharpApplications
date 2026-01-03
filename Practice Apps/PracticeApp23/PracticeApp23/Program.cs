65dd96534cb8d6b5344eb15bce91848b9c5cd843
Corne Stols <cornestols7@gmail.com>
2026-01-03 11:27:46 +0200

    New Commit

---
+++ Practice Apps/PracticeApp23/PracticeApp23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PracticeApp23
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+
+            Console.WriteLine($"Person constructor: {name}");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+
+        public Employee(string name, int id) : base(name)
+        {
+            ID = id;
+
+            Console.WriteLine($"Employee constructor: {name}, ID: {id}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee("John Doe", 123);
+        }
+    }
+}
