6a9b18f37b89c2d92ff53fe9e931bdd43f0adae7
Cornelius <cornestols7@gmail.com>
2026-01-05 12:43:08 +0200

    New Commit

---
+++ Practice Apps/PracticeApp25/PracticeApp25/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PracticeApp25
+{
+    internal class Animal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("The animal makes a sound.");
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public new void Speak()
+        {
+            Console.WriteLine("The dog barks.");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            myAnimal.Speak();
+
+            Dog myDog = new Dog();
+            myDog.Speak();
+
+            Animal myPet = new Dog();
+            myPet.Speak();
+        }
+    }
+}
