fcdabdd9f92b2ebfa020987e74d8576d0aee783e
Cornelius <cornestols7@gmail.com>
2026-01-07 15:37:47 +0200

    New Commit

---
+++ Practice Apps/PracticeApp26/PracticeApp26/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PracticeApp26
+{
+    public class Vehicle
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+
+        public virtual void ShowMessage()
+        {
+            Console.WriteLine("Test Message 01");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+
+        public override sealed void ShowMessage()
+        {
+            Console.WriteLine("Test Message 02");
+        }
+    }
+
+    public class Bike : Car
+    {
+        public int BikeId { get; set; }
+
+        public override void ShowMessage() //Error because sealed method in Car class
+        {
+            Console.WriteLine("Test Message 03");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+
+            vehicle.ShowMessage();
+
+            Car car = new Car();
+
+            car.ShowMessage();
+
+            Bike bike = new Bike();
+
+            bike.ShowMessage();
+        }
+    }
+}
---
+++ Practice Apps/PracticeApp27/PracticeApp27/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PracticeApp27
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public virtual void ShowMessage()
+        {
+            Console.WriteLine("Message 01");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+
+        public override void ShowMessage()
+        {
+            base.ShowMessage();
+            Console.WriteLine("Message 02");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+
+            vehicle.ShowMessage();
+
+            Car car = new Car();
+
+            car.ShowMessage();
+        }
+    }
+}
