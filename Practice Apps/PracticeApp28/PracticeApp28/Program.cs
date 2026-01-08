d221a689e97e42c1549e60d751eaf223ae320786
Cornelius <cornestols7@gmail.com>
2026-01-08 09:14:29 +0200

    New Commit

---
+++ Practice Apps/PracticeApp28/PracticeApp28/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PracticeApp28
+{
+    internal abstract class Animal
+    {
+        public abstract void MakeSound();
+
+        public void Sleep()
+        {
+            Console.WriteLine("Zzz...");
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof! Woof!");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Animal animal = new Animal(); //Cant instantiate cause abstract class
+            Dog dog = new Dog();
+            dog.MakeSound();
+            dog.Sleep();
+        }
+    }
+}
