0a493b2d3f05db5f01c74920c209b97edf6d8909
Cornelius <cornestols7@gmail.com>
2026-01-20 14:54:58 +0200

    New Commit

---
+++ Practice Apps/PracticeApp29/PracticeApp29/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace PracticeApp29
+{
+    public interface IAnimal
+    {
+        void MakeSound();
+
+        void Eat(string food);
+    }
+
+    public class Dog : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Barks");
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"Dog ate {food}");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Meeoow!!");
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"Cat ate {food}");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.MakeSound();
+            dog.Eat("Treat");
+
+            Cat cat = new Cat();
+            cat.MakeSound();
+            cat.Eat("Fish");
+        }
+    }
+}
