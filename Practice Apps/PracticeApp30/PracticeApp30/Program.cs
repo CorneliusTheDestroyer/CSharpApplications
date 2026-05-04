4df3ef65ec77e4bafa3ffe9bbc9bcc4a7b0439af
Corne Stols <cornestols7@gmail.com>
2026-05-04 18:41:05 +0200

    New Commit

---
+++ Practice Apps/PracticeApp30/PracticeApp30/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Serialization;
+
+namespace PracticeApp30
+{
+
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardProcessor: IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing credit card payment: {amount}");
+        }
+    }
+
+    public class PayPalProcessor: IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing PayPal payment: {amount}");
+        }
+    }
+
+    public class PaymentService
+    {
+        private readonly IPaymentProcessor _processor;
+
+        public PaymentService(IPaymentProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public void ProcessOrderPayment(decimal amount)
+        {
+            _processor.ProcessPayment(amount);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPaymentProcessor creditcardProcessor = new CreditCardProcessor();
+            PaymentService paymentService = new PaymentService(creditcardProcessor);
+            paymentService.ProcessOrderPayment(100.00m);
+
+            IPaymentProcessor paypalProcessor = new PayPalProcessor();
+            paymentService = new PaymentService(paypalProcessor);
+            paymentService.ProcessOrderPayment(200.00m);
+
+        }
+    }
+}
---
+++ Practice Apps/PracticeApp31 - Polymorphism with Interfaces Part 1/PracticeApp31/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PracticeApp31
+{
+    public interface IPayment
+    {
+        // TODO: Declare a method ProcessPayment
+        void ProcessPayment();
+    }
+
+    public class CreditCardPayment : IPayment
+    {
+        // TODO: Implement the ProcessPayment method
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Processing credit card payment");
+        }
+    }
+
+    public class PayPalPayment : IPayment
+    {
+        // TODO: Implement the ProcessPayment method
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Processing PayPal payment");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            // TODO: Create instances of CreditCardPayment and PayPalPayment, and call the ProcessPayment method on each
+            IPayment creditCardPayment = new CreditCardPayment();
+            creditCardPayment.ProcessPayment();
+
+            IPayment payPalPayment = new PayPalPayment();
+            payPalPayment.ProcessPayment();
+        }
+    }
+}
