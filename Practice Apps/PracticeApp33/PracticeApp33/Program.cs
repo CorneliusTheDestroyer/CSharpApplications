7d26d1c6231146e1ddc9075095cba9999553774a
Corne Stols <cornestols7@gmail.com>
2026-05-05 11:27:11 +0200

    New Commit

---
+++ Practice Apps/PracticeApp33/PracticeApp33/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PracticeApp33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Logs";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.AppendAllText(filePath, "Hello World" + "\n");
+        }
+    }
+}
