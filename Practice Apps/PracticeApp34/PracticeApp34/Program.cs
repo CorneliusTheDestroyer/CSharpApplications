a0e6f31b5a8ed8f92db37648f663ae9f87843413
Corne Stols <cornestols7@gmail.com>
2026-05-05 11:57:03 +0200

    New Commit

---
+++ Practice Apps/PracticeApp34/PracticeApp34/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace PracticeApp34
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class FileLogger: ILogger
+    {
+        public void Log(string message)
+        {
+            string directoryPath = @"C:\Logs";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.AppendAllText(filePath, message + "\n");
+        }
+    }
+
+    public class DatabaseLogger: ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Logging to database. {message}");
+        }
+    }
+
+    public class Application
+    {
+        private readonly ILogger _logger;
+
+        public Application(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoWork()
+        {
+            _logger.Log("Work started.");
+            _logger.Log("Work completed.");
+        }
+    }
+
+    /*
+     Decoupling: The Application class depends on the 
+    ILogger interface rather than specific implementations
+    like FileLogger or DatabaseLogger.
+    This means you can easily switch the logging mechanism
+    without changing the Appilcation class.
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger fileLogger = new FileLogger();
+            Application app = new Application(fileLogger);
+            app.DoWork();
+
+            ILogger dbLogger = new DatabaseLogger();
+            app = new Application(dbLogger);
+            app.DoWork();
+        }
+    }
+}
