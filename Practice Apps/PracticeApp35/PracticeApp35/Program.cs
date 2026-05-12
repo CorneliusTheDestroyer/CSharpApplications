0114f1fb2145d2645a1ab4ab494cb7163a1ed59e
Corne Stols <cornestols7@gmail.com>
2026-05-12 10:28:14 +0200

    New Commit

---
+++ Practice Apps/PracticeApp35/PracticeApp35/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+
+namespace PracticeApp35
+{
+    public interface IEmployee
+    {
+        void Task();
+    }
+
+    public class Programmer : IEmployee
+    {
+        public void Task() 
+        {
+            Console.WriteLine("Employee does Programming.");
+        }
+    }
+
+    public class Manager : IEmployee
+    {
+        public void Task()
+        {
+            Console.WriteLine("Employee manages other employees.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmployee programmer = new Programmer();
+
+            programmer.Task();
+
+            IEmployee manager = new Manager();
+
+            manager.Task();
+        }
+    }
+}
